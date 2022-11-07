0f2fd0e0f5561f63288bddf2b9bd22e0547d567f
varadib02 <91455828+varadib02@users.noreply.github.com>
2022-11-07 10:39:40 +0100

    clone

---
+++ 22-11/22-11-07.py
@@ -0,0 +1,12 @@
+import random
+sz=[]
+szclone=[]
+for i in range(10):
+    sz.append(random.randint(-10,10))
+
+szclone.append=sz
+
+print(szclone)
+for i in range(len(hiba)):
+    del szclone[hiba[i]]
+print(sz)
