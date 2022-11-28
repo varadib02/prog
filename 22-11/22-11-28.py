c79d9300054e864617e837f06ab4e1c291a1ae6c
varadib02 <91455828+varadib02@users.noreply.github.com>
2022-11-28 09:35:08 +0100

    atlag

--- 22-11/22-11-28.py
+++ 22-11/22-11-28.py
@@ -4,10 +4,10 @@ lszam=[]
 lszoveg=[]
 
 for i in range(len(adat)):
-    #száme?
-    if(adat[i].isnumeric()==True):lszam.append(adat[i])
+    #szám-e?
+    if(adat[i].isnumeric()==True):lszam.append(int(adat[i]))
     #szöveg-e
     if(adat[i].isalpha()==True): lszoveg.append(adat[i])
-
 print(lszam)
 print(lszoveg)
+print(round(sum(lszam)/len(lszam),2))
