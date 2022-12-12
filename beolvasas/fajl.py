3f4ace9dbc4649a16a7cbfd90bec51a1edc832aa
varadib02 <91455828+varadib02@users.noreply.github.com>
2022-12-12 11:24:57 +0100

    beo

---
+++ beolvasas/fajl.py
@@ -0,0 +1,22 @@
+from random import *
+
+fajl=open("beolvasas/szamok.txt","w")
+for i in range(10):
+    fajl.write(str(randint(1,100))+"\n")
+fajl.close()
+
+fajl=open("beolvasas/szamok.txt","r")
+#a /n sornak veszi a print
+#egymas alá
+lszam=[]
+for i in fajl:
+    i=i.rstrip()
+    lszam.append(i)
+    print(i.rstrip())
+
+print(lszam)
+#egymasmelé
+fajl=open("beolvasas/szamok.txt","r")
+for i in fajl:
+    i=i.rstrip()
+    print(i,end=" ")
