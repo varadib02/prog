d75c9fe2e53a8d7503356ccd8e25b4b4c255ba16
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-01-09 11:36:24 +0100

    Create teglalap.py

---
+++ eljaras/teglalap.py
@@ -0,0 +1,19 @@
+
+def be():
+    a=int(input())
+    b=int(input())
+    terulet(a,b)
+
+def terulet(a,b):
+    T=a*b
+    K=2*(a+b)
+    kiiars(T,K)
+
+def kiiars(T,K):
+    print(f"Terület: {T}")
+    print(f"Kerület: {K}")
+
+
+be()
+
+
