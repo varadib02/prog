ee202d110ce15de316b7f9d2931fa9559aff1cee
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-01-09 12:43:51 +0100

    Update dolgozat.py

--- eljaras/dolgozat.py
+++ eljaras/dolgozat.py
@@ -4,12 +4,10 @@ def be():
     
     print(f"{megfelte(maxpont,pont)}")
     if megfelte(maxpont,pont):
-        print(f"{jegy(maxpont,pont)}%")
+        print(f"{round(pont/maxpont*100,2)}% {pont} ponttal")
 
 def megfelte(maxpont,pont):
     if maxpont/2<=pont:
         return "Átment"
     else: return "Nem sikerült" 
-def jegy(maxpont,pont):
-    return
 be()
